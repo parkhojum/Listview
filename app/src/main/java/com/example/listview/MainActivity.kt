7cefcb8954d9be1e41a53563b2597a99b4312023
박호준 <hankil417@naver.com>
2020-07-06 21:39:46 +0900

    실제 데이터 추가

--- app/src/main/java/com/example/listview/MainActivity.kt
+++ app/src/main/java/com/example/listview/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.listview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listview.datas.Student
 
 class MainActivity : AppCompatActivity() {
+
+    val mStudentList = ArrayList<Student>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add(Student("박호준","인천시 부평구",1990))
+        mStudentList.add(Student("조경진","서울시 중량구",1988))
+        mStudentList.add(Student("김재영","서울시 은평구",1986))
+        mStudentList.add(Student("이에원","서울시 금천구",1984))
+        mStudentList.add(Student("조장우","서울시 종로구",1983))
+        mStudentList.add(Student("채정실","서울시 용산구",1991))
+        mStudentList.add(Student("김미희","서울시 중량구",1995))
+
     }
 }
